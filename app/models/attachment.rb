6e77029a2792e52044f55488cab88327fd781809
Joshua Muheim <josh@muheimwebdesign.ch>
2015-10-19 17:40:06 +0200

    New stuff

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,4 +3,6 @@ class Attachment < ActiveRecord::Base
   mount_base64_uploader :string_upload, ImageUploader
 
   belongs_to :user
+
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
   mount_base64_uploader :string_upload, ImageUploader
 
   has_many :attachments
-  accepts_nested_attributes_for :attachments, allow_destroy: true
+  accepts_nested_attributes_for :attachments, allow_destroy: true, reject_if: :all_blank
+
+  validates :name, presence: true
 end
